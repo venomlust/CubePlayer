271ddf8d608a6c791603e5d23e0849b1f21499ed
Lucas Bertollo <Lucas Bertollo>
2016-09-15 03:30:36 -0300

    fix

--- assets/js/home.js
+++ assets/js/home.js
@@ -84,6 +84,38 @@ angular.module("appModule", ['angular.filter']).controller("cubePlayerCtrl", fun
         });
     };
 
+    //UI fixes
+    $('#music-timeline').css('width', '100%');
+    $('#music-volume').css('width', '100%');
+
+    $('#library').height($(window).height() - $('#header').height());
+
+    $(window).resize(function() {
+        $('#library').height($(window).height() - $('#header').height());
+
+        if (Modernizr.mq('only screen and (max-width: 644px)')) {
+            $('#basic-controls').removeClass('col-xs-4').addClass('col-xs-12');
+            $('#realtime-info').removeClass('col-xs-8').addClass('col-xs-12');
+            $('#music-volume').css('width', '50%');
+        }
+        if (Modernizr.mq('only screen and (min-width: 644px)')) {
+            $('#basic-controls').removeClass('col-xs-12').addClass('col-xs-4');
+            $('#realtime-info').removeClass('col-xs-12').addClass('col-xs-8');
+            $('#music-volume').css('width', '100%');
+        }
+    });
+
+    /* Fix drag n drop error */
+    $('#library').on('dragover', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+    });
+
+    $('#header').on('dragover', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+    });
+
     /* On DropFiles */
     $('#library').on('drop', function(event) {
         event.stopPropagation();
